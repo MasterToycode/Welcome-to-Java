719ec41faf58e393daa87fb41b80842c5de0053e
叶俊泽 <128799728+MasterToycode@users.noreply.github.com>
2024-03-07 00:06:39 +0800

    初次提交

---
+++ src/Wechat/ClientThread.java
@@ -0,0 +1,44 @@
+package Wechat;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class ClientThread extends Thread{
+    @Override
+    public void run() {
+        try (InputStream is = socket.getInputStream();
+        ) {
+
+            DataInputStream dataInputStream = new DataInputStream(is);
+            while (true)
+            {
+
+                try {
+                    String s = dataInputStream.readUTF();
+                    System.out.println(s);
+
+                } catch (IOException e) {
+                    System.out.println(socket.getLocalAddress().getHostAddress()+" is left!");
+                    dataInputStream.close();
+                    is.close();
+                    socket.close();
+                    break;
+                }
+
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ClientThread(Socket socket){
+        this.socket=socket;
+    }
+
+    private Socket socket;
+
+
+}
